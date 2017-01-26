9f5805d68c36ebdb13a7535a528e9fe95f5d865f
Nicholas Mertin <nmertin@mertin.home>
2017-01-25 20:57:13 -0500

    Added current display

---
+++ Climber Current Display/ComponentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel.Composition;
+using Frc1360.DriverStation.RobotComm;
+using Frc1360.DriverStation.RobotComm.ComponentModel;
+
+namespace Frc1360.DriverStation.Components.ClimberCurrent
+{
+    [Export(typeof(IComponentController))]
+    [ExportMetadata("Name", "Climber Current Display")]
+    public sealed class ComponentController : IComponentController
+    {
+        private Display display;
+
+        public CurrentController Controller;
+
+        public UIElement Display => display;
+
+        public Task InitializeAsync(Connection connection) => Task.Run(() =>
+            {
+                Application.Current.Dispatcher.Invoke(() => display = new Display());
+                Controller = new CurrentController(connection, display);
+            });
+    }
+}
---
+++ Climber Current Display/CurrentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frc1360.DriverStation.RobotComm;
+
+namespace Frc1360.DriverStation.Components.ClimberCurrent
+{
+    public sealed class CurrentController : ControllerBase
+    {
+        private static Stream s;
+        private Display d;
+        private ObservableCollection<float> data = new ObservableCollection<float>();
+
+        public CurrentController(Connection conn, Display display) : base(conn, 1, out s)
+        {
+            d = display;
+            Task.Run(() =>
+            {
+                using (var r = new BinaryReader(s))
+                    while (true)
+                    {
+                        var c = r.ReadSingle();
+                        d.Dispatcher.Invoke(() =>
+                        {
+                            d.current.Text = $"{c} A";
+                            data.Add(c);
+                            if (data.Count > 50)
+                                data.RemoveAt(0);
+                        });
+                    }
+            });
+        }
+    }
+}
