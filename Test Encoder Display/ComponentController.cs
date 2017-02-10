332fc5ff1a00519dd13e9ed19583effeece36eea
Nicholas Mertin <nmertin@mertin.home>
2017-02-09 19:58:13 -0500

    Started test encoder display

---
+++ Test Encoder Display/ComponentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Frc1360.DriverStation.RobotComm;
+using Frc1360.DriverStation.RobotComm.ComponentModel;
+
+namespace Frc1360.DriverStation.Components.TestEncoderDisplay
+{
+    [Export(typeof(IComponentController))]
+    [ExportMetadata("Name", "Test Encoder Display")]
+    public class ComponentController : IComponentController
+    {
+        private Display display;
+
+        public UIElement Display => display;
+
+        public Task InitializeAsync(Connection connection) => Task.Run(() =>
+        {
+
+        });
+    }
+}
---
+++ Test Encoder Display/EncoderController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frc1360.DriverStation.RobotComm;
+using Frc1360.DriverStation.RobotComm.Utilities;
+
+namespace Frc1360.DriverStation.Components.TestEncoderDisplay
+{
+    public class EncoderController : ControllerBase
+    {
+        [ThreadStatic]
+        private static Stream tempStream;
+
+        private Stream stream;
+
+        public EncoderController(Connection conn, Display display) : base(conn, 2, out tempStream)
+        {
+            stream = tempStream;
+            Task.Run(() =>
+            {
+                using (var r = new BinaryReader(stream))
+                    while (true)
+                    {
+                        
+                    }
+            });
+        }
+    }
+}
