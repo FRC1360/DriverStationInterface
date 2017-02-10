dfc2afccc7b7e87eb69fbd8ec481ee2bf7a21b22
Nicholas Mertin <nmertin@mertin.home>
2017-02-10 16:05:33 -0500

    Updated IntermediaryStream to possibly fix thread deadlock issue

--- Robot Communication Interface/Utilities/IntermediaryStream.cs
+++ Robot Communication Interface/Utilities/IntermediaryStream.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Frc1360.DriverStation.RobotComm.Utilities
 {
@@ -11,6 +11,9 @@ namespace Frc1360.DriverStation.RobotComm.Utilities
     {
         Stream baseStream;
         Func<Exception, Stream> handler;
+        object rl = new object(), wl = new object(), ml = new object();
+        EventWaitHandle ewh = new EventWaitHandle(true, EventResetMode.ManualReset);
+        HashSet<Thread> currentThreads = new HashSet<Thread>();
 
         public IntermediaryStream(Stream baseStream, Func<Exception, Stream> handler)
         {
@@ -18,57 +21,86 @@ namespace Frc1360.DriverStation.RobotComm.Utilities
             this.handler = handler ?? throw new ArgumentNullException(nameof(handler));
         }
 
-        private void Try(Action action)
+        private void Try(object _lock, Action action)
         {
-            lock (this)
-                while (true)
+            while (true)
+                try
                 {
-                    try
-                    {
-                        action();
-                        return;
-                    }
-                    catch (Exception e)
-                    {
-                        baseStream = handler(e);
-                    }
+                    lock (currentThreads)
+                        currentThreads.Add(Thread.CurrentThread);
+                    ewh.WaitOne();
+                    lock (_lock)
+                        try
+                        {
+                            action();
+                            return;
+                        }
+                        catch (Exception e)
+                        {
+                            ewh.Reset();
+                            lock (currentThreads)
+                            {
+                                foreach (var t in currentThreads)
+                                    t.Interrupt();
+                                currentThreads.Clear();
+                            }
+                            Thread.Yield();
+                            while (true)
+                                try
+                                {
+                                    baseStream = handler(e);
+                                    break;
+                                }
+                                catch
+                                {
+                                    continue;
+                                }
+                            ewh.Set();
+                        }
+                }
+                catch (ThreadInterruptedException)
+                {
+                    continue;
+                }
+                finally
+                {
+                    lock (currentThreads)
+                        if (currentThreads.Contains(Thread.CurrentThread))
+                            currentThreads.Remove(Thread.CurrentThread);
                 }
         }
 
-        private T Try<T>(Func<T> action)
+        private T Try<T>(object _lock, Func<T> action)
         {
-            lock (this)
-                while (true)
-                {
-                    try
-                    {
-                        return action();
-                    }
-                    catch (Exception e)
-                    {
-                        baseStream = handler(e);
-                    }
-                }
+            object result = null;
+            Try(_lock, new Action(() => result = action()));
+            return (T)result;
         }
 
-        public override bool CanRead => Try(() => baseStream.CanRead);
+        public override bool CanRead => Try(ml, () => baseStream.CanRead);
 
-        public override bool CanSeek => Try(() => baseStream.CanSeek);
+        public override bool CanSeek => Try(ml, () => baseStream.CanSeek);
 
-        public override bool CanWrite => Try(() => baseStream.CanWrite);
+        public override bool CanWrite => Try(ml, () => baseStream.CanWrite);
 
-        public override long Length => Try(() => baseStream.Length);
+        public override long Length => Try(ml, () => baseStream.Length);
 
-        public override long Position { get => Try(() => baseStream.Position); set => Try(() => baseStream.Position = value); }
+        public override long Position
+        {
+            get => Try(ml, () => baseStream.Position);
+            set => Try(ml, () => baseStream.Position = value);
+        }
 
-        public override void Flush() => Try(baseStream.Flush);
+        public override void Flush() => Try(wl, baseStream.Flush);
 
-        public override int Read(byte[] buffer, int offset, int count) => Try(() => baseStream.Read(buffer, offset, count));
+        public override int Read(byte[] buffer, int offset, int count) => Try(rl, () => baseStream.Read(buffer, offset, count));
 
-        public override long Seek(long offset, SeekOrigin origin) => Try(() => baseStream.Seek(offset, origin));
+        public override long Seek(long offset, SeekOrigin origin) => Try(ml, () => baseStream.Seek(offset, origin));
 
-        public override void SetLength(long value) => Try(() => baseStream.SetLength(value));
+        public override void SetLength(long value) => Try(ml, () => baseStream.SetLength(value));
 
-        public override void Write(byte[] buffer, int offset, int count) => Try(() => baseStream.Write(buffer, offset, count));
+        public override void Write(byte[] buffer, int offset, int count) => Try(wl, () => baseStream.Write(buffer, offset, count));
+
+        protected override void Dispose(bool disposing) => ewh.Dispose();
     }
 }
